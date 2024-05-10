474a8e156e2140a2957109820cd4ebd1a08a623f
Squashies <19284840+joshuajcarter@users.noreply.github.com>
2024-05-10 12:35:58 +0100

    edits to index.js add comments for interactivity

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// import mocks  from 'mocks.js'
 const {collection} = require('./mocks')
 
-// console.log(mocks)
+// helper filters collection by release year
 function getAllReleases(num) {
-  let res = mocks.filter((el) => el.Released === num);
+  let res = collection.filter((el) => el.Released === num);
   return res
 }
 
+// create index.html onload input instruction
 function getYearRange(arr) {
   let range = []
   arr.forEach(el =>  {if (el.Released > 0) range.push(el.Released)})
@@ -15,13 +15,14 @@ function getYearRange(arr) {
   let rangeEnd = range[range.length - 1];
   return `Pick a year between ${rangeStart} - ${rangeEnd}`
 }
+
+// executed on index.html button click
 function getRandomItem(num) {
   let res = getAllReleases(num)
   let random = res[Math.floor(Math.random() * res.length)];
+  console.log(`Here's a selection from ${num}`)
   return { Artist: random.Artist, Title: random.Title }
 }
 
-// console.log(getAllReleases(1997).length)
-// console.log(getRandomItem(1997))
 console.log(getYearRange(collection))
-// console.log(mocks[0])
+console.log(getRandomItem(1988))
